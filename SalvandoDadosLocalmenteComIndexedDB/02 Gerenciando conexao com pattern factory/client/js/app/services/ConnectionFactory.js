7086b38f656d65a9f90ee964d12e77954d99576f
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-09-19 19:00:19 -0300

    finalização do capitulo

--- SalvandoDadosLocalmenteComIndexedDB/02 Gerenciando conexao com pattern factory/client/js/app/services/ConnectionFactory.js
+++ SalvandoDadosLocalmenteComIndexedDB/02 Gerenciando conexao com pattern factory/client/js/app/services/ConnectionFactory.js
@@ -1,9 +1,10 @@
 var ConnectionFactory = (function (){
-  var stores = ['negociacoes'];
-  var version =4 
-  var dbName = 'aluraframe'
+  const stores = ['negociacoes'];
+  const version =4 
+  const dbName = 'aluraframe'
   
   var connection = null
+  var close = null 
   
   return  class ConnectionFactory{
     constructor(){
@@ -25,6 +26,10 @@ var ConnectionFactory = (function (){
         openRequest.onsuccess = e => {
           if(!connection){
             connection = e.target.result
+            close = connection.close.bind(connection)
+            connection.close = function(){
+              throw new Error('Você não pode fechar diretamente a conexao')
+            }
           }
   
           resolve(connection)
@@ -44,6 +49,13 @@ var ConnectionFactory = (function (){
         }
         connection.createObjectStore(store,{autoIncrement: true})
       })
+    }
+
+    static _closeConnection(){
+      if(connection){
+        close()
+        connection = null
+      }
     }
   }
 })()
